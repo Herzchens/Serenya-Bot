bb2874ba32d2b4a8910c8af6a5453510f09132e7
regilariousmathiew-tech <regilariousmathiew@gmail.com>
2026-06-28 01:03:59 +0100

    Delete src directory
    i added the updated folder inside the old folder

--- src/discord/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod checks;
-pub mod embeds;
-pub mod pagination;
--- src/src/logging/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod redact;
-pub mod webhook;
-
-pub use redact::{MakeRedactingWriter, redact_secrets, register_secret_to_redact};
